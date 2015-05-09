db5b1ce79d697cb42ba9910a10c999fe3050251f
grafi <grafi@grafi.jp>
2015-05-10 06:07:33 +0900

    move test files

--- test/decodeparse/test.sh
+++ test/decodeparse/test.sh
@@ -2,7 +2,7 @@
 source ../luabin.sh
 
 echo "# valid"
-for l in *-decoder.lua; do
+for l in all-*-decoder.lua test-*-decoder.lua; do
 	echo "## $l"
 	for j in validjson/*.json; do
 		echo "### ${j}"
@@ -18,7 +18,7 @@ for l in *-decoder.lua; do
 done
 
 echo "# invalid"
-for l in *-decoder.lua; do
+for l in all-*-decoder.lua test-*-decoder.lua; do
 	echo "## $l"
 	for j in invalidjson/*.json; do
 		echo "### ${j}"
@@ -39,7 +39,7 @@ for r in *.rb; do
 	ruby "${r}" > "${r%.rb}.json"
 done
 cd ..
-for l in *-decoder.lua; do
+for l in all-*-decoder.lua bench-*-decoder.lua; do
 	echo "## $l"
 	for j in benchjson/*.json; do
 		echo "### ${j}"
