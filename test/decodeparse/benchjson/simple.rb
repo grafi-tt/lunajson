74ec32214b8933c9946ae5ab7c52b846d95b781f
Shunsuke Shimizu <shunsuke@is.s.u-tokyo.ac.jp>
2015-05-10 17:50:56 +0900

    change benchmark

--- test/decodeparse/benchjson/simple.rb
+++ test/decodeparse/benchjson/simple.rb
@@ -1,7 +1,7 @@
 puts '['
 10000.times do |i|
   f = i.to_f
-  puts %Q!\t{"x":#{9*f+f/100000},"y":#{-f+f/100000},"#{"mmm%xaaaaaaaa"%(i%1000)}":#{[true,'"foobar"'][i%2]},"iiiii":#{i}},!
+  puts %Q!\t{"x":#{9*f+f/100000},"y":#{-f+f/100000},"string":"#{"mmm%xaaaaaaaa"%(i%1000)}","value":#{[true,'"foo\nbar"',i][i%3]}},!
 end
 puts 'false'
 puts ']'
