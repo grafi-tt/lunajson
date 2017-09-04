0e5713cc82618f9798096e884f8283698f599478
Shunsuke Shimizu <grafi@grafi.jp>
2017-09-04 14:19:20 +0900

    improve efficiency and error reporting of surrogate pair decoding (#12)

--- src/lunajson/decoder.lua
+++ src/lunajson/decoder.lua
@@ -195,7 +195,7 @@ local function newdecoder()
 		['f']  = '\f',
 		['n']  = '\n',
 		['r']  = '\r',
-		['t']  = '\t'
+		['t']  = '\t',
 	}
 	f_str_escapetbl.__index = function()
 		decodeerror("invalid escape sequence")
@@ -203,59 +203,81 @@ local function newdecoder()
 	setmetatable(f_str_escapetbl, f_str_escapetbl)
 
 	local f_str_surrogate_prev = 0
-	local function f_str_subst(ch, rest)
+	local function f_str_subst(ch, ucode)
 		if ch == 'u' then
-			local c1, c2, c3, c4 = byte(rest, 1, 4)
-			local ucode = f_str_hextbl[c1-47] * 0x1000 +
-			              f_str_hextbl[c2-47] * 0x100 +
-			              f_str_hextbl[c3-47] * 0x10 +
-			              f_str_hextbl[c4-47]
-			if ucode == inf then
-				myerror("invalid unicode charcode")
-			end
-			rest = sub(rest, 5)
-			if ucode < 0x80 then -- 1byte
-				return char(ucode) .. rest
-			elseif ucode < 0x800 then -- 2byte
-				c1 = floor(ucode / 0x40)
-				c2 = ucode - c1 * 0x40
-				return char(0xC0 + c1, 0x80 + c2) .. rest
-			elseif ucode < 0xD800 or 0xE000 <= ucode then -- 3byte
-				c1 = floor(ucode / 0x1000)
-				ucode = ucode - c1 * 0x1000
-				c2 = floor(ucode / 0x40)
-				c3 = ucode - c2 * 0x40
-				return char(0xE0 + c1, 0x80 + c2, 0x80 + c3) .. rest
-			elseif 0xD800 <= ucode and ucode < 0xDC00 then -- surrogate pair 1st
-				if f_str_surrogate_prev == 0 then
-					f_str_surrogate_prev = ucode
-					if rest == '' then
-						return ''
+			local c1, c2, c3, c4, rest = byte(ucode, 1, 5)
+			ucode = f_str_hextbl[c1-47] * 0x1000 +
+			        f_str_hextbl[c2-47] * 0x100 +
+			        f_str_hextbl[c3-47] * 0x10 +
+			        f_str_hextbl[c4-47]
+			if ucode ~= inf then
+				if ucode < 0x80 then -- 1byte
+					if rest then
+						return char(ucode, rest)
 					end
-				end
-			else -- surrogate pair 2nd
-				if f_str_surrogate_prev == 0 then
-					f_str_surrogate_prev = 1
-				else
-					ucode = 0x10000 +
-					        (f_str_surrogate_prev - 0xD800) * 0x400 +
-					        (ucode - 0xDC00)
+					return char(ucode)
+				elseif ucode < 0x800 then -- 2byte
+					c1 = floor(ucode / 0x40)
+					c2 = ucode - c1 * 0x40
+					c1 = c1 + 0xC0
+					c2 = c2 + 0x80
+					if rest then
+						return char(c1, c2, rest)
+					end
+					return char(c1, c2)
+				elseif ucode < 0xD800 or 0xE000 <= ucode then -- 3byte
+					c1 = floor(ucode / 0x1000)
+					ucode = ucode - c1 * 0x1000
+					c2 = floor(ucode / 0x40)
+					c3 = ucode - c2 * 0x40
+					c1 = c1 + 0xE0
+					c2 = c2 + 0x80
+					c3 = c3 + 0x80
+					if rest then
+						return char(c1, c2, c3, rest)
+					end
+					return char(c1, c2, c3)
+				elseif 0xD800 <= ucode and ucode < 0xDC00 then -- surrogate pair 1st
+					if f_str_surrogate_prev == 0 then
+						f_str_surrogate_prev = ucode
+						if not rest then
+							return ''
+						end
+						decodeerror("1st surrogate pair byte not continued by 2nd")
+					end
 					f_str_surrogate_prev = 0
-					c1 = floor(ucode / 0x40000)
-					ucode = ucode - c1 * 0x40000
-					c2 = floor(ucode / 0x1000)
-					ucode = ucode - c2 * 0x1000
-					c3 = floor(ucode / 0x40)
-					c4 = ucode - c3 * 0x40
-					return char(0xF0 + c1, 0x80 + c2, 0x80 + c3, 0x80 + c4) .. rest
+					decodeerror("two contiguous 1st surrogate pair bytes")
+				else -- surrogate pair 2nd
+					if f_str_surrogate_prev ~= 0 then
+						ucode = 0x10000 +
+								(f_str_surrogate_prev - 0xD800) * 0x400 +
+								(ucode - 0xDC00)
+						f_str_surrogate_prev = 0
+						c1 = floor(ucode / 0x40000)
+						ucode = ucode - c1 * 0x40000
+						c2 = floor(ucode / 0x1000)
+						ucode = ucode - c2 * 0x1000
+						c3 = floor(ucode / 0x40)
+						c4 = ucode - c3 * 0x40
+						c1 = c1 + 0xF0
+						c2 = c2 + 0x80
+						c3 = c3 + 0x80
+						c4 = c4 + 0x80
+						if rest then
+							return char(c1, c2, c3, c4, rest)
+						end
+						return char(c1, c2, c3, c4)
+					end
+					decodeerror("2nd surrogate pair byte appeared without 1st")
 				end
 			end
+			decodeerror("invalid unicode codepoint literal")
 		end
 		if f_str_surrogate_prev ~= 0 then
 			f_str_surrogate_prev = 0
-			decodeerror("invalid surrogate pair")
+			decodeerror("1st surrogate pair byte not continued by 2nd")
 		end
-		return f_str_escapetbl[ch] .. rest
+		return f_str_escapetbl[ch] .. ucode
 	end
 
 	-- caching interpreted keys for speed
@@ -291,10 +313,10 @@ local function newdecoder()
 		end
 		local str2 = str
 		if find(str2, '\\', 1, true) then -- check if backslash occurs
-			str2 = gsub(str2, '\\(.)([^\\]*)', f_str_subst) -- interpret escapes
+			str2 = gsub(str2, '\\(.)([^\\]?[^\\]?[^\\]?[^\\]?[^\\]?)', f_str_subst) -- interpret escapes
 			if f_str_surrogate_prev ~= 0 then
 				f_str_surrogate_prev = 0
-				decodeerror("invalid surrogate pair")
+				decodeerror("1st surrogate pair byte not continued by 2nd")
 			end
 		end
 		if iskey then -- commit key cache
