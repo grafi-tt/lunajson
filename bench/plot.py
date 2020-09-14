1dcf3fadd001a7d75673a4354fcbf16ce72c5cdb
Shunsuke Shimizu <grafi@grafi.jp>
2020-09-14 10:11:27 +0900

    encode color

--- bench/plot.py
+++ bench/plot.py
@@ -8,40 +8,42 @@ import yaml
 
 LIB_NAMES = {
     'decode': {
-        'lunajson': "lunajson decoder",
-        'lunajson_sax': "lunajson SAX",
-        'dkjson_lpeg': "dkjson with lpeg",
-        'dkjson_pure': "dkjson w/o lpeg",
-        'cjson': "Lua CJSON",
+        'lunajson': ("lunajson decoder", 'C0'),
+        'lunajson_sax': ("lunajson SAX", 'C1'),
+        'dkjson_pure': ("dkjson w/o lpeg", 'C2'),
+        'dkjson_lpeg': ("dkjson with lpeg", 'C3'),
+        'cjson': ("Lua CJSON", 'C4'),
     },
     'encode': {
-        'lunajson': "lunajson",
-        'dkjson': "dkjson",
-        'cjson': "Lua CJSON",
+        'lunajson': ("lunajson", 'C0'),
+        'dkjson': ("dkjson", 'C2'),
+        'cjson': ("Lua CJSON", 'C4'),
     },
 }
 
 
 def load_result(f):
     lua_impls = []
+    colors = {}
     data_dict = defaultdict(lambda: defaultdict(list))
 
     for lua_result in yaml.safe_load_all(f):
         lua_impls.append(lua_result['lua_impl'])
         for op in ('decode', 'encode'):
             for lib, lib_result in lua_result[op].items():
-                libname = LIB_NAMES[op][lib]
+                libname, color = LIB_NAMES[op][lib]
+                colors[libname] = color
                 for task, time in lib_result.items():
                     filename = op + '-' + task
                     data_dict[filename][libname].append(time)
 
-    return lua_impls, data_dict
+    return lua_impls, colors, data_dict
 
 
-def plot_data(filename, lua_impls, data):
+def plot_data(filename, lua_impls, colors, data):
     dpi = 96
-    plt.style.use('tableau-colorblind10')
     fig, ax = plt.subplots(figsize=(800/dpi, 400/dpi))
+    plt.style.use('tableau-colorblind10')
 
     if 'decode' in filename:
         ax.set_title("Decoding Performances")
@@ -59,7 +61,8 @@ def plot_data(filename, lua_impls, data):
     for i, (libname, times) in enumerate(data.items()):
         barcenter = (i - 0.5 * (len(data) - 1)) * groupwidth / len(data)
         barwidth = groupwidth / len(data)
-        ax.bar(xticks + barcenter, times, width=barwidth, label=libname)
+        ax.bar(xticks + barcenter, times,
+               color=colors[libname], width=barwidth, label=libname)
 
     ax.legend()
     fig.tight_layout()
@@ -71,9 +74,9 @@ def main():
     mpl.use('Agg')
 
     with open('bench_result.yml') as f:
-        lua_impls, data_dict = load_result(f)
+        lua_impls, colors, data_dict = load_result(f)
         for filename, data in data_dict.items():
-            plot_data(filename, lua_impls, data)
+            plot_data(filename, lua_impls, colors, data)
 
 
 if __name__ == '__main__':
